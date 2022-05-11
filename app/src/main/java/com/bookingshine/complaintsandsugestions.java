9acc1978d2e5342f4babd9eba79002990db384c5
carhumdante <carhumdante@gmail.com>
2022-05-11 07:44:59 -0500

    Nav_graph

--- app/src/main/java/com/bookingshine/complaintsandsugestions.java
+++ app/src/main/java/com/bookingshine/complaintsandsugestions.java
@@ -3,10 +3,12 @@ package com.bookingshine;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +62,14 @@ public class complaintsandsugestions extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_complaintsandsugestions, container, false);
+        //final View view = inflater.inflate(R.layout.fragment_complaintsandsugestions, container, false);
+
+        //Button red = view.findViewById(R.id.button15);
+        //red.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+                //Navigation.findNavController(view).navigate(R.id.red);
+        //   }
+        //});
     }
 }

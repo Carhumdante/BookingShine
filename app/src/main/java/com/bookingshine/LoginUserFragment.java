b544a963b12dfb81b82312a267764ef5c3351b55
Kartoffelpuree <alanruiz0411@gmail.com>
2022-05-30 00:38:48 -0500

    Login complete with register and close session
    Signed-off-by: Kartoffelpuree <alanruiz0411@gmail.com>

--- app/src/main/java/com/bookingshine/LoginUserFragment.java
+++ app/src/main/java/com/bookingshine/LoginUserFragment.java
@@ -1,15 +1,28 @@
 package com.bookingshine;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginUserFragment extends Fragment {
+    private FirebaseAuth mAuth;
+    private EditText editTextEmailU;
+    private EditText getEditTextPasswordU;
+    private Button loginuser1;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -48,13 +61,30 @@ public class LoginUserFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login_user, container, false);
-        Button loginUser = view.findViewById(R.id.loginuser1);
-        loginUser.setOnClickListener(new View.OnClickListener() {
+
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmailU = view.findViewById(R.id.editTextEmailU);
+        getEditTextPasswordU = view.findViewById(R.id.editTextPasswordU);
+        loginuser1 = view.findViewById(R.id.loginuser1);
+        loginuser1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_loginUserFragment_to_home_User);
-            }
+                String email = editTextEmailU.getText().toString().trim();
+                String password = getEditTextPasswordU.getText().toString().trim();
 
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Navigation.findNavController(view).navigate(R.id.action_loginUserFragment_to_home_User);
+                        }
+                        else {
+                            Log.d("Tag","Email or password incorrect", task.getException());
+                            Toast.makeText(getActivity(),"Email or password incorrect.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
         });
         Button regisUser = view.findViewById(R.id.RegUser1);
         regisUser.setOnClickListener(new View.OnClickListener() {
